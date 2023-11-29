53ae17c9e0ff4d8aeadace914af14215f6c56784
Junstory <lastcom1@gmail.com>
2023-11-30 01:17:26 +0900

    [FEAT] 미션도전API 구현

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,17 @@
+//user.controller.js
 import { response } from "../../config/response.js";
 import { status } from "../../config/response.status.js";
 
-import { joinUser } from "../services/user.service.js";
+import { joinUser, joinMission } from "../services/user.service.js";
 
 export const userRegister = async (req, res, next) => {
     console.log("회원가입을 요청하였습니다!");
     //console.log("body:", req.body); // 값이 잘 들어오나 찍어보기 위한 테스트용
 
     res.send(response(status.SUCCESS, await joinUser(req.body)));
+}
+
+export const userMission = async (req,res,next) =>{
+    console.log("미션을 도전합니다!");
+    res.send(response(status.SUCCESS, await joinMission(req.body)));
 }
--- src/dtos/user.dto.js
+++ src/dtos/user.dto.js
@@ -2,7 +2,10 @@
 
 // Register response DTO
 export const RegisterResponseDTO = (user) => {
-    console.log("DTO성공");
-    console.log(user[0]);
+
     return {"email": user[0].login_id, "name": user[0].name};
+}
+
+export const MissionResponseDTO = (mission) =>{
+    return {"mission_id": mission[0].mission_id, "member_id": mission[0].member_id};
 }
