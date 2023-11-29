4f5138715c97bbf5ad64a0a21ca6023da026eac4
Junstory <lastcom1@gmail.com>
2023-11-29 10:18:57 +0900

    mission9 test

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ import { response } from './config/response';
 import { status } from './config/response.status';
 import { BaseError } from './config/error';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express()
 const port = 3000
+// server setting - veiw, static, body-parser etc..
+app.set('port', process.env.PORT || 3000)   // 서버 포트 지정                            // cors 방식 허용
+app.use(express.static('public'));          // 정적 파일 접근
+app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
+app.use(express.urlencoded({extended: false})); // 단순 객체 문자열 형태로 본문 데이터 해석
+
 
 const myLogger = (req,res,next) => {
     console.log("LOGGED");
