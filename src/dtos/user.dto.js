8bc691558acf2d81e4af09d3670880c121e63267
Junstory <lastcom1@gmail.com>
2023-12-06 23:33:41 +0900

    [FEAT] 내가 작성한 리뷰 목록 API

--- src/dtos/user.dto.js
+++ src/dtos/user.dto.js
@@ -9,3 +9,23 @@ export const RegisterResponseDTO = (user) => {
 export const MissionResponseDTO = (mission) =>{
     return {"mission_id": mission[0].mission_id, "member_id": mission[0].member_id};
 }
+
+//reviews
+export const previewReviewResponseDTO = (data) => {
+    const reviews = [];
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+        reviews.push({
+            "shop_name": data[i].name,
+            "rating": data[i].rating,
+            "description": data[i].description,
+            "create_date": formatDate(data[i].created_at)
+        })
+    }
+    console.log(2);
+    return {"reviewData": reviews, "cursorId": data[data.length-1].id};
+}
+
+const formatDate = (date) => {
+    return new Intl.DateTimeFormat('ko').format(new Date(date)).replaceAll(" ","").slice(0,-1);
+}
